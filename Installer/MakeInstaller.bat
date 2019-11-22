5fc8bafeee6fc1f4b850de4fafef2d6f78856237
CarJem Generations <Cwallacecs@Gmail.com>
2019-11-21 22:23:19 -0500

    Finalized Building of Installer into the Repo

---
+++ Installer/MakeInstaller.bat
@@ -0,0 +1,4 @@
+SET scriptLocation="D:\Users\CarJem\source\repos\Sonic3AIR_ModManager\Installer\Sonic 3 A.I.R. Mod Manager.nsi"
+SET makeNISIWLocation="C:\Program Files (x86)\NSIS\makensisw.exe"
+
+%makeNISIWLocation% /V1 %scriptLocation%
