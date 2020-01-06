f8069bb6c7cbce2e8455272722b07ba309966c30
CarJem Generations <Cwallacecs@Gmail.com>
2020-01-05 21:25:12 -0500

    Finalizing Build for Version 20.1.5.0

--- Sonic3AIR_ModManager/Properties/AssemblyInfo.cs
+++ Sonic3AIR_ModManager/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Security.Permissions;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("20.01.01.3")]
-[assembly: AssemblyFileVersion("20.01.01.3")]
+[assembly: AssemblyVersion("20.01.05.0")]
+[assembly: AssemblyFileVersion("20.01.05.0")]
 [assembly: Guid("74887941-ce31-48e7-a4ef-75c52ec6415e")]
 
