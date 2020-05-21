d6ea25ef3e333042bc8e30210ea813c16c494182
CarJem Generations <Cwallacecs@Gmail.com>
2020-05-21 16:40:45 -0400

    Small Cleanup

--- Installer/MakeInstaller.bat
+++
@@ -1,5 +0,0 @@
-SET scriptLocation="D:\Users\CarJem\source\sonic3air_repos\Sonic3AIR_ModManager\Installer\Sonic 3 A.I.R. Mod Manager.nsi"
-SET makeNISIWLocation="C:\Program Files (x86)\NSIS\makensisw.exe"
-SET Version=%1
-
-%makeNISIWLocation% /V4 /DVersion=%Version% %scriptLocation%
